e06d83fc52c84c524e364e3a11e67c4bb6058946
cyjSS <92037407+cyjSS@users.noreply.github.com>
2021-12-23 23:43:41 +0900

    Update Main_S4_10816_숫자카드2_HashMap.java

--- baekjoon/Main_S4_10816_숫자카드2_HashMap.java
+++ baekjoon/Main_S4_10816_숫자카드2_HashMap.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.StringTokenizer;
 
-public class Main_S4_10816_최영진_HashMap {
+public class Main_S4_10816_HashMap {
 
 	static int N,M,cnt;
 	public static void main(String[] args)throws Exception {
