532776690c377cabc74b703f4cbf8e44e553fa6b
cyjSS <92037407+cyjSS@users.noreply.github.com>
2021-12-23 23:44:15 +0900

    Update Main_S3_9461_파도반수열.java

--- baekjoon/Main_S3_9461_파도반수열.java
+++ baekjoon/Main_S3_9461_파도반수열.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.StringReader;
 
-public class Main_S3_9461_최영진 {
+public class Main_S3_9461 {
 
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
