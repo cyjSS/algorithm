3360edee60ab0690184ff7270e911fbc458cd0a0
cyjSS <92037407+cyjSS@users.noreply.github.com>
2021-12-23 23:35:43 +0900

    Update Main_B1_2999_비밀이메일.java

--- baekjoon/Main_B1_2999_비밀이메일.java
+++ baekjoon/Main_B1_2999_비밀이메일.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.StringReader;
 
-public class Main_B1_2999_ {
+public class Main_B1_2999_비밀이메일 {
 
 	public static void main(String[] args)throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
