c7f3c16198fa7efcbde44bf28ff2aa430b24edd1
cyjSS <92037407+cyjSS@users.noreply.github.com>
2021-12-23 23:42:42 +0900

    Update Main_S5_1010_다리놓기.java

--- baekjoon/Main_S5_1010_다리놓기.java
+++ baekjoon/Main_S5_1010_다리놓기.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.io.StringReader;
 import java.util.StringTokenizer;
 
-public class Main_S5_1010_최영진 {
+public class Main_S5_1010 {
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		br = new BufferedReader(new StringReader(src));
