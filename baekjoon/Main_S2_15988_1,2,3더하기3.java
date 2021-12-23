6e7ebc6117c7f35a0762cb68c8eb08d732471c22
cyjSS <92037407+cyjSS@users.noreply.github.com>
2021-12-23 23:48:43 +0900

    Update Main_S2_15988_1,2,3더하기3.java

--- baekjoon/Main_S2_15988_1,2,3더하기3.java
+++ baekjoon/Main_S2_15988_1,2,3더하기3.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.StringReader;
 
-public class Main_S2_15988_최영진 {
+public class Main_S2_15988 {
 
 	public static void main(String[] args)throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
